b9618525ab04b512280e57b0621361786e07753d
Zahra Ghaderi <z.ghaderizefreh75@gmail.com>
2019-10-23 15:52:58 +0330

    Updates

---
+++ precache-manifest.356f60266a2be4a03787358cc5a8ffbc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8ef7008016dd21bae11bcb1ac06bde0",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "fad3516b75608c8666f4",
+    "url": "/resume/static/css/main.aa2cb8ea.chunk.css"
+  },
+  {
+    "revision": "5f3717a76ef6a36240b9",
+    "url": "/resume/static/js/2.8ddbab5e.chunk.js"
+  },
+  {
+    "revision": "fad3516b75608c8666f4",
+    "url": "/resume/static/js/main.845c3419.chunk.js"
+  },
+  {
+    "revision": "6a3898924d9bed645e57",
+    "url": "/resume/static/js/runtime-main.988921d5.js"
+  }
+]);
